45bb1a524bff28be46a6d5115ea051f103d39df8
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-10-04 00:37:14 +0100

    generated recipe model and corresponding recipes table in database for rails application

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_03_232552) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_03_233456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_url"
+    t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
