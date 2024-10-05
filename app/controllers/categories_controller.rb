9784da2858a2163eed636ddd873b70dfe4249585
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-10-05 15:17:14 +0100

    basic routing for the category model and categories table using the routes controller model views approach

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,33 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.save
+
+    redirect_to categories_path
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path, status: :see_other
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
