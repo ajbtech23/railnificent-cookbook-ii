5176e5d87b159627a973a825e8506324b4af3f17
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-10-05 14:20:12 +0100

    worked through my associations and validations and populated recipes and categories tables

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :categories, through: :bookmarks
+
+  validates :description, :name, presence: true
+  validates :name, uniqueness: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 10.0 }
 end
